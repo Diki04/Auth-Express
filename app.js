850876fa4c52700231e3ba3d5e2b584b0706371d
Diki04 <sinyofamily152520@gmail.com>
2024-09-30 10:33:28 +0700

    Connect DB

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 import express from "express";
 import authRoutes from "./routes/authRoutes.js";
+import 'dotenv/config';
+import mongoose from "mongoose";
 
 const app = express();
 const port = 3000;
 
 app.use('/api/v1/auth', authRoutes);
+
+// Koneksi DB
+try {
+  await mongoose.connect('process.env.DATABASE');
+  console.log("Database connected");
+} catch (error) {
+  handleError(error);
+  console.log("Database not connected");
+}
 
 app.listen(port, () => {
   console.log("Server running on port 3000");
