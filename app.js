5296c5c87b2acc17ef36bf0e9086b0b1c9a41f48
Diki04 <sinyofamily152520@gmail.com>
2024-11-03 01:02:56 +0700

    errorHandling

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import authRoutes from "./routes/authRoutes.js";
 import 'dotenv/config';
 import mongoose from "mongoose";
+import { errorHandler,notFoundPath } from "./middleware/errorMiddleware.js";
 
 const app = express();
 const port = 3000;
@@ -9,6 +10,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/v1/auth', authRoutes);
+
+app.use(notFoundPath)
+app.use(errorHandler)
 
 // Koneksi DB
 try {
