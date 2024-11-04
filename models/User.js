a7539e38a72dfdac7b41875bdd675f859aab2175
Diki04 <sinyofamily152520@gmail.com>
2024-11-04 23:42:03 +0700

    bycrpt

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import validator from 'validator'
+import bcrypt from 'bcryptjs'
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
@@ -36,5 +37,11 @@ const userSchema = new Schema({
   }
   
 });
+
+userSchema.pre("save",async function(){
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
+
 const User = mongoose.model("User", userSchema)
 export default User;
